28b5baaa37f8a27f54a95c2c64484d11e72fb630
13010-34-16 <13010-34-16@exchange.ad.afpanet>
2023-10-12 16:32:00 +0200

    calculator js finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,15 +14,88 @@ let actualCalculation = ""
 let lastCalculation = ""
 let operation = undefined
 
+let calculate = () => {
+    let calculation 
+    if (!lastCalculation ||! actualCalculation) {
+        return
+    }
+    let last = parseFloat(lastCalculation)
+    let actual = parseFloat(actualCalculation)
+
+    if(isNaN(last) || isNaN(actual)){
+        return
+    }
+
+    switch (operation) {
+        case "+":
+            calculation = last + actual
+            break;
+        case "-":
+            calculation = last - actual
+            break;
+        case "×":
+            calculation = last * actual
+            break;
+        case "÷":
+            calculation = last / actual
+            if (actual === 0) {
+                alert("ERROR!!!")
+                return
+            }
+            break;
+        default:
+            return;
+        }
+
+    actualCalculation = calculation
+    operation = undefined 
+    lastCalculation = ""
+
+}
+
+
+
+
+
+let selectOperation = (operator) => {
+    if (actualCalculation === "") {
+        return
+    }
+    if (lastCalculation !==""){
+        calculate()
+    }
+    operation = operator
+    lastCalculation = actualCalculation
+    actualCalculation = " "
+}
+
+
+
+
 //  1. funkcja wprowadzania tekstu do ekranu
 const updateScore = () => {
     mainScreen.innerText = actualCalculation
-    secondScreen.innerText = lastCalculation
+    if (operation != null) {
+        secondScreen.innerText = lastCalculation + operation
+    }else {
+        secondScreen.innerText = ''
+    }
+    
 }
 
 //  2. funkcja dodawania liczb do ekranu
 let addNumber = (calcNr) => {
+    if (calcNr === ",") {
+        if (actualCalculation.includes(",")) {
+            return;
+        }
+        calcNr = ","
+    }
     actualCalculation = actualCalculation.toString() + calcNr.toString(); 
+}
+
+let deleteNr = () => {
+    actualCalculation = actualCalculation.toString().slice(0, -1)
 }
 
 numbers.forEach((calcNr) => {
@@ -31,3 +104,43 @@ numbers.forEach((calcNr) => {
         updateScore()
     })
 })
+
+del.addEventListener('click', () => {
+    deleteNr()
+    updateScore()
+})
+
+let cleanAll = () => {
+    actualCalculation = " "
+}
+clean.addEventListener('click', () =>{
+    cleanAll()
+    updateScore()
+} )
+
+let cleanScore = () => {
+    actualCalculation = ""
+    lastCalculation = ""
+    operation = undefined
+}
+
+operators.forEach((operator) =>{
+    operator.addEventListener('click', () => {
+        selectOperation(operator.innerText)
+        updateScore()
+    })
+})
+
+equals.addEventListener("click", () => {
+    calculate()
+    updateScore()
+    
+})
+
+clean.addEventListener('click', () => {
+    cleanScore()
+    updateScore()
+})
+
+
+
